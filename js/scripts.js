8ad22fe42d813a4a22839b0cd32d040364a1762a
Mara Timberlake <maratimberlake@msn.com>
2017-06-16 14:53:15 -0700

    update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+//business logic
 function toggleSection(sectionName) {
   var button = $('#' + sectionName)
   if (button.css('display') === 'none') {
@@ -7,65 +8,50 @@ function toggleSection(sectionName) {
   }
 }
 
-
-function Pizza(size, cheese, sauce, topping){
-  this.size = size;
-  this.cheese = cheese;
+function Pizza (size, sauce, cheese, topping, name){
+  this.name = name;
   this.sauce = sauce;
+  this.cheese = cheese;
   this.topping = topping;
+  this.size = size;
 }
 
-Pizza.prototype.priceReview = function() {
-  this.price = 9;
-  if(this.size === 'medium') {
-    this.price +=2;
-  } else if(this.size === 'large') {
-    this.price +=4;
+Pizza.prototype.priceCalculator = function() {
+  this.price = 10;
+  if (this.size === 'medium') {
+    this.price += 2;
+  } else if (this.size === 'large') {
+    this.price += 4;
   }
-
-  for (var i = 0; this.cheese.length; i++) {
-    this price +=1;
+  for (var i = 0; i < this.cheese.length; i++) {
+  this.price += 1;
   }
-
-  for (var i = 0; this.topping.length; i++) {
-    this price +=1;
+  for (var i = 0; i < this.topping.length; i++) {
+  this.price += 1;
   }
 }
 
+var displayOrderResults = function() {
+  $('.output').show();
+}
 
+ //interface logic
+ $(document).ready(function() {
+   $("form#new-pizza").submit(function(event) {
+     event.preventDefault();
+     var size = $('input[name=size]:checked').val();
+     var sauce = $('input[name=sauce]:checked').val();
+     var topping = $('input:checkbox[name=topping]:checked').map(function() {
+         return this.value;
+       }).get();
+     var cheese = $('input:checkbox[name=cheese]:checked').map(function() {
+         return this.value;
+       }).get();
 
+     newPizza.priceCalculator();
 
-
-
-
-
-
-
-
-// $(document).ready(function() {
-//   var resultLabel = $('#quiz-result');
-//
-//   $("#pizza-form").submit(function(event) {
-
-//   })
-// });
-
-
-$(document).ready(function() {
-  $('#pizza-form').submit(function(event) {
-    event.preventDefault();
-    $('#user-input').css('display', 'none');
-    $('#output').css('display', 'block');
-
-    var size = $('input[name=size]:checked').val();
-    var sauce = $('input[name=sauce]:checked').val();
-    var cheese = $('input:checkbox[name=cheese]:checked').map(function() {
-        return this.number;
-      }).get();
-    var topping = $('input:checkbox[name=topping]:checked').map(function() {
-        return this.number;
-      }).get();
-    var newPizza = new Pizza(size, sauce, cheese, toppings);
-    newPizza.priceReview();
-    });
-  });
+     $(".pizza").last().click(function() {
+       $('#pizza-details').show();
+     });
+   });
+ });
