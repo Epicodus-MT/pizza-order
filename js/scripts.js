d9daf3b9aa4eb2313b4b0bf585b942cd43b78942
Mara Timberlake <maratimberlake@msn.com>
2017-06-16 16:23:14 -0700

    edit scripts.js, refactor html code, fix indentation

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,60 @@
 //business logic
-function toggleSection(sectionName) {
-  var button = $('#' + sectionName)
-  if (button.css('display') === 'none') {
-    button.css('display', 'block');
-  } else {
-    button.css('display', 'none');
-  }
+// function toggleSection(sectionName) {
+//   var button = $('#' + sectionName)
+//   if (button.css('display') === 'none') {
+//     button.css('display', 'block');
+//   } else {
+//     button.css('display', 'none');
+//   }
+// }
+
+
+//business logic
+function Pizza (size, sauce, cheese, topping){
+  this.size = size;
+  this.sauce = sauce;
+  this.cheese = cheese;
+  this.topping = topping;
+  this.price = 10;
 }
 
-//interface logic
-// $(document).ready(function() {
-//
-//  $("#pizza-form").submit(function(event) {
-//
-//    $('#user-input').css('display', 'none');
-//    $('#output').css('display', 'block');
-//  })
-// });
+Pizza.prototype.isPizzaTotal = function() {
+  if (this.size === "medium") {
+    this.price += 2;
+  }else if (this.size === "large") {
+    this.price += 4;
+  }
+
+  // for ...
+
+  return this.price;
+}
+
+//user interface logic
+$(document).ready (function () {
+  debugger;
+  $("form#new-pizza").submit(function (event) {
+    event.preventDefault();
+
+    // $("#purchase span").each(function() {
+    //   $(this).empty();
+    // });
+
+    // var inputtedname = $("input#name").val();
+    var checkedSize = $("input[name='size']:checked").val();
+    var checkedSauce = $("input[name='sauce']:checked").val();
+    var checkedCheese = $("input[name='cheese']:checked").val();
+    var checkedTopping = $("input[name='topping']:checked").val();
+
+
+
+    var myPizza = new Pizza(checkedSize, checkedSauce, checkedCheese, checkedTopping);
+    $(".size").append(myPizza.size);
+    $(".sauce").append(myPizza.sauce);
+    $(".cheese").append(myPizza.cheese);
+    $(".topping").append(myPizza.topping);
+    $(".price").append(myPizza.isPizzaTotal());
+
+
+  })
+});
