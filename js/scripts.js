93c8d79b86aaecf4b8d5e22fb46f492e309c2b6a
Mara Timberlake <maratimberlake@msn.com>
2017-06-16 12:38:43 -0700

    edit blockquote and change oveall style

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,15 @@
+$(document).ready(function() {
+  var resultLabel = $('#quiz-result');
+
+  $("#pizza-form").submit(function(event) {
+    $('#user-input').css('display', 'none');
+    $('#output').css('display', 'block');
+  })
+});
+
+
+
+
 
 function toggleSection(sectionName) {
   var button = $('#' + sectionName)
