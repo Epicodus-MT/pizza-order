c8fe1d194cbe6cdcf9528d831a4cdb7f0cbbde6d
Mara Timberlake <maratimberlake@msn.com>
2017-06-20 17:37:48 -0700

    fix 'About Us' button

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,12 @@
 //business logic
-// function toggleSection(sectionName) {
-//   var button = $('#' + sectionName)
-//   if (button.css('display') === 'none') {
-//     button.css('display', 'block');
-//   } else {
-//     button.css('display', 'none');
-//   }
-// }
+function toggleSection(sectionName) {
+  var button = $('#' + sectionName)
+  if (button.css('display') === 'none') {
+    button.css('display', 'block');
+  } else {
+    button.css('display', 'none');
+  }
+}
 
 
 //business logic
@@ -17,7 +17,7 @@ function Pizza (size, sauce, cheese, topping){
   this.topping = topping;
 }
 
-Pizza.prototype.isPizzaTotal = function() {
+Pizza.prototype.pizzaTotal = function() {
   this.price = 10;
   if (this.size === "medium") {
     this.price += 2;
@@ -32,10 +32,10 @@ Pizza.prototype.isPizzaTotal = function() {
   }
   return this.price;
 }
-
-Pizza.prototype.result = function() {
-  return "Size" + this.size + ", Sauce" + this.sauce + ", Cheese(s)" + this.cheese + ", Topping(s)" + this.topping + "=  Price: $" + this.price;
-}
+//
+// Pizza.prototype.result = function() {
+//   return "Size" + this.size + ", Sauce" + this.sauce + ", Cheese(s)" + this.cheese + ", Topping(s)" + this.topping + "=  Price: $" + this.price;
+// }
 
 
 //user interface logic
@@ -48,8 +48,13 @@ $(document).ready (function () {
     // var inputtedname = $("#name").val();
     var checkedSize = $("input[name='size']:checked").val();
     var checkedSauce = $("input[name='sauce']:checked").val();
-    var checkedCheese = $("input[name='cheese']:checked").val();
-    var checkedTopping = $("input[name='topping']:checked").val();
+    // var checkedCheese = $("input[name='cheese']:checked").val();
+    var checkedCheeses = $("input[name='cheese']:checked").map(function(index, input) {
+      return $(input).val();
+    }).get();
+    var checkedToppings = $("input[name='topping']:checked").map(function(index, input) {
+      return $(input).val();
+    }).get();
 
     console.log ({
       'name': name,
@@ -62,12 +67,12 @@ $(document).ready (function () {
     var resultText = '';
 
 
-    var myPizza = new Pizza(checkedSize, checkedSauce, checkedCheese, checkedTopping);
+    var myPizza = new Pizza(checkedSize, checkedSauce, checkedCheeses, checkedToppings);
     $(".size").append(myPizza.size);
     $(".sauce").append(myPizza.sauce);
-    $(".cheese").append(myPizza.cheese);
-    $(".topping").append(myPizza.topping);
-    $(".price").append(myPizza.isPizzaTotal());
+    $(".cheese").append(myPizza.cheese.join(', '));
+    $(".topping").append(myPizza.topping.join(', '));
+    $(".purchase-price").append(myPizza.pizzaTotal());
 
 
     $('#new-pizza').css('display', 'none');
